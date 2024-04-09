811936490bca93bdcad64b2d1b45abcf23798db7
Terper <jann.totterman@gmail.com>
2024-04-09 10:38:14 +0000

    ändring

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
-PORT=8888 # Ändra till din egen port!
 
 app = Flask(__name__)
 CORS(app) 
@@ -12,6 +11,7 @@ app.config['JSON_AS_ASCII'] = False
 
 load_dotenv()
 db_url = os.environ.get("DB_URL")
+port = os.environ.get("PORT")
 
 @app.route("/")
 def index():
@@ -23,7 +23,7 @@ def index():
 # - Ändra portnummer
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
+    app.run(host='0.0.0.0', port=port, debug=True, ssl_context=(
         '/etc/letsencrypt/fullchain.pem', 
         '/etc/letsencrypt/privkey.pem'
     ))
