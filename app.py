9b46127336bf72a047b856df6ee2d241d58325f0
Terper <jann.totterman@gmail.com>
2024-05-04 01:52:54 +0000

    the rest

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ port = os.environ.get("PORT")
 
 @app.before_request
 def before_request():
+    # ignores auth check for cors
+    if request.method == "OPTIONS":
+        return
     # check if header inludes authorization
     api_key = request.headers.get("Authorization")
     if api_key == None:
@@ -33,19 +36,76 @@ def index():
 
 @app.route("/todos", methods=["GET", "POST"])
 def todos():
-    return
+    api_key = request.headers.get("Authorization")
+    if request.method == "GET":
+        with psycopg.connect(db_url, row_factory=dict_row) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                            SELECT t.id, t.title, t.done, t.due_date, c.name as category
+                            FROM
+                                wdbcms24_todo t
+                                INNER JOIN wdbcms24_category c ON t.category_id = c.id
+                            WHERE
+                                t.user_id = (SELECT id FROM wdbcms24_users WHERE api_key = %s)
+                            """, [api_key])
+                rows = cur.fetchall()
+                return rows
+    else:
+        with psycopg.connect(db_url, row_factory=dict_row) as conn:
+            with conn.cursor() as cur:
+                category = request.json.get("category")
+                title = request.json.get("title")
+                due_date = request.json.get("due_date")
+                # since due_date is optional
+                if due_date != None:
+                    cur.execute("""
+                                INSERT INTO wdbcms24_todo (user_id, category_id, title, due_date) VALUES (
+                                    (SELECT id FROM wdbcms24_users WHERE api_key = %s),
+                                    (SELECT id FROM wdbcms24_category WHERE name = %s),
+                                    %s,
+                                    %s
+                                )""", [api_key, category, title, due_date])
+                else:
+                    cur.execute("""
+                                INSERT INTO wdbcms24_todo (user_id, category_id, title) VALUES (
+                                    (SELECT id FROM wdbcms24_users WHERE api_key = %s),
+                                    (SELECT id FROM wdbcms24_category WHERE name = %s),
+                                    %s
+                                )""", [api_key, category, title])
+                return Response(status=200)
 
 @app.route("/todos/<id>", methods=["PUT", "DELETE"])
 def modify_todo(id):
-    return
+    api_key = request.headers.get("Authorization")
+    if request.method == "PUT":
+        with psycopg.connect(db_url, row_factory=dict_row) as conn:
+            with conn.cursor() as cur:
+                done = request.json.get("done")
+                cur.execute("""
+                            UPDATE wdbcms24_todo
+                            SET done = %s
+                            WHERE
+                                id = %s AND
+                                user_id = (SELECT id FROM wdbcms24_users WHERE api_key = %s)
+                            """, [done, id, api_key])
+                return Response(status=200)
+    else:
+        with psycopg.connect(db_url, row_factory=dict_row) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                            DELETE FROM wdbcms24_todo
+                            WHERE
+                                id = %s AND
+                                user_id = (SELECT id FROM wdbcms24_users WHERE api_key = %s)
+                            """, [id, api_key])
+                return Response(status=200)
 
 @app.route("/categories", methods=["GET"])
 def categories():
-        with psycopg.connect(db_url) as conn:
+        with psycopg.connect(db_url, row_factory=dict_row) as conn:
             with conn.cursor() as cur:
-                cur.execute("SELECT name FROM wdbcms24_category")
+                cur.execute("SELECT * FROM wdbcms24_category")
                 rows = cur.fetchall()
-                print(rows[0])
                 return rows
 
 ## Kom ihåg:
